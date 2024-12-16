aa65b45077c9eaa727b0e0a2b7d57f30bc4518cb
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2024-12-16 18:22:49 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,3 +1,14 @@
+import sys
+sys.path.insert(0, "release-code")
+
+import os
+
+# get release from environment variable
+version = os.environ.get("VERSION", "")
+if not version:
+    print("Error: VERSION environment variable not set.")
+    sys.exit(1)
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -9,12 +20,14 @@
 project = 'Deeplay'
 copyright = '2024, The Deeplay Team'
 author = 'The Deeplay Team'
-release = 'version'
+release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_automodapi.automodapi", "sphinx.ext.githubpages"]
+numpydoc_show_class_members = False
+automodapi_inheritance_diagram = False
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +37,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_theme_options = {
+    "switcher": {
+        "json_url": "https://ORGANIZATION.github.io/REPO/latest/_static/switcher.json",
+        "version_match": version,
+    },
+    "navbar_end": [
+        "version-switcher",
+        "navbar-icon-links",
+    ],
+}
